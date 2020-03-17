4f03f3d04d08074364b4799b80fd47697687063b
xdtalx <dortal14@gmail.com>
2020-03-17 17:27:33 +0200

    Added scripts for CI\CD pipeline

--- engine/server.js
+++ engine/server.js
@@ -1,4 +1,4 @@
-const app = require('./engine/app');
+const app = require('./app');
 const debug = require('debug')('node-angular');
 const http = require('http');
 const port = normalizePort(process.env.PORT || 3000);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://alguru-env.eba-rbgpyt4r.eu-central-1.elasticbeanstalk.com/api'
 };
