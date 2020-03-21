9f907f38fd33985751b620f6630803da69bb7434
xdtalx <dortal14@gmail.com>
2020-03-21 17:55:21 +0200

    Updated dependecies

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const questionsRoutes = require('./routes/questions');
 const usersRoutes = require('./routes/users');
 
-mongoose.connect("mongodb+srv://Admin:" + process.env.MONGO_ATLAS_PASSWORD + "@cluster0-obtib.mongodb.net/Alguru?retryWrites=true&w=majority")
+mongoose.connect(process.env.MONGO_AUTH)
   .then(() => {
     console.log('Connected to database.');
   })
