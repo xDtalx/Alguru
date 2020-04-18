239f44fe73fffc5a4f74ea54f9119c697b632f9f
xdtalx <dortal14@gmail.com>
2020-04-18 18:11:39 +0300

    Deleted get template routes

--- backend/controllers/code.js
+++ backend/controllers/code.js
@@ -1,17 +1,5 @@
 const request = require('request');
 
-exports.getTemplate = (req, res, next) => {
-  const url = process.env.RUN_CODE_API + '/template?lang=' + req.params.lang;
-
-  request.get(url, null, (err, runCodeRes, body) => {
-    if(err) {
-      res.status(500).json({ message: 'Unknown error' });
-      return;
-    }
-    res.status(runCodeRes.statusCode).send(body);
-  });
-};
-
 exports.executeCode = (req, res, next) => {
   const data = {
     lang: req.body.lang,
