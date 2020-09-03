fedbfeb46d473adab33eff554109e4aa84e8227d
dortal94 <dortal14@gmail.com>
2020-09-03 21:15:17 +0300

    Changed settings button to an icon

--- client/src/app/profile/profile.module.ts
+++ client/src/app/profile/profile.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFacebook, faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faUser } from '@fortawesome/free-solid-svg-icons';
 import { CropperComponent } from '../image-cropper/image-cropper.component';
+import { SharedModule } from '../notifications-shared.module';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
-import { SharedModule } from '../notifications-shared.module';
 
 @NgModule({
   declarations: [ProfileComponent, CropperComponent],
@@ -16,6 +16,6 @@ import { SharedModule } from '../notifications-shared.module';
 export class ProfileModule {
   constructor(library: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
-    library.addIcons(faFacebook, faLinkedin, faGithub, faTwitter, faUser);
+    library.addIcons(faFacebook, faLinkedin, faGithub, faTwitter, faUser, faCog);
   }
 }
